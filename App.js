38243e7a7bbe6868cb2bc548e767152bbd048eed
masunaga <masunaga.yudai@tis.co.jp>
2022-06-09 00:41:04 +0900

    finish section4

--- App.js
+++ App.js
@@ -1,14 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { StyleSheet, View, Image, Text } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +7,51 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  itemContainer: {
+    height: 100,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    flexDirection: 'row',
+  },
+  leftContainer: {
+    width: 100,
+  },
+  rightContainer: {
+    flex: 1,
+    padding: 10,
+    justifyContent: 'space-between',
+  },
+  text: {
+    fontSize: 16,
+  },
+  subText: {
+    fontSize: 12,
+    color: 'gray',
+  },
 });
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.itemContainer}>
+        <View style={styles.leftContainer}>
+          <Image
+            style={{ height: 100, width: 100 }}
+            source={{ uri: 'https://picsum.photos/id/10/200/200' }}
+          />
+        </View>
+        <View style={styles.rightContainer}>
+          <Text style={styles.text} numberOfLines={3}>
+            orem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation
+          </Text>
+          <Text style={styles.subText}>ReactNews</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default App;
